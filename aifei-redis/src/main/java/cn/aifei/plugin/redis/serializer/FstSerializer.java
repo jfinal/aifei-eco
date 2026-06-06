a44016d919bfb8e9e34ebf9edbbc09b4456cf146
zhanbo <jfinal@126.com>
2026-06-06 22:15:58 +0800

    feat: add aifei-redis

--- aifei-redis/src/main/java/cn/aifei/plugin/redis/serializer/FstSerializer.java
+++ aifei-redis/src/main/java/cn/aifei/plugin/redis/serializer/FstSerializer.java
@@ -28,71 +28,67 @@ import redis.clients.jedis.util.SafeEncoder;
  * FstSerializer.
  */
 public class FstSerializer implements Serializer {
-	
-	public static final Serializer me = new FstSerializer();
-	
-	public byte[] keyToBytes(String key) {
-		return SafeEncoder.encode(key);
-	}
-	
-	public String keyFromBytes(byte[] bytes) {
-		return SafeEncoder.encode(bytes);
-	}
-	
-	public byte[] fieldToBytes(Object field) {
-		return valueToBytes(field);
-	}
-	
-    public Object fieldFromBytes(byte[] bytes) {
-    	return valueFromBytes(bytes);
+
+    public static final Serializer me = new FstSerializer();
+
+    public byte[] keyToBytes(String key) {
+        return SafeEncoder.encode(key);
     }
-	
-	public byte[] valueToBytes(Object value) {
-		FSTObjectOutput fstOut = null;
-		try {
-			ByteArrayOutputStream bytesOut = new ByteArrayOutputStream();
-			fstOut = new FSTObjectOutput(bytesOut);
-			fstOut.writeObject(value);
-			fstOut.flush();
-			return bytesOut.toByteArray();
-		}
-		catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-		finally {
-			if (fstOut != null) {
-				try {
-					fstOut.close();
-				} catch (IOException e) {
-					Log.get(getClass()).error(e.getMessage(), e);
-				}
-			}
-		}
-	}
-	
-	public Object valueFromBytes(byte[] bytes) {
-		if(bytes == null || bytes.length == 0) {
-			return null;
-		}
-		
-		FSTObjectInput fstInput = null;
-		try {
-			fstInput = new FSTObjectInput(new ByteArrayInputStream(bytes));
-			return fstInput.readObject();
-		}
-		catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-		finally {
-			if (fstInput != null) {
-				try {
-					fstInput.close();
-				} catch (IOException e) {
-					Log.get(getClass()).error(e.getMessage(), e);
-				}
-			}
-		}
-	}
+
+    public String keyFromBytes(byte[] bytes) {
+        return SafeEncoder.encode(bytes);
+    }
+
+    public byte[] fieldToBytes(Object field) {
+        return valueToBytes(field);
+    }
+
+    public Object fieldFromBytes(byte[] bytes) {
+        return valueFromBytes(bytes);
+    }
+
+    public byte[] valueToBytes(Object value) {
+        FSTObjectOutput fstOut = null;
+        try {
+            ByteArrayOutputStream bytesOut = new ByteArrayOutputStream();
+            fstOut = new FSTObjectOutput(bytesOut);
+            fstOut.writeObject(value);
+            fstOut.flush();
+            return bytesOut.toByteArray();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (fstOut != null) {
+                try {
+                    fstOut.close();
+                } catch (IOException e) {
+                    Log.get(getClass()).error(e.getMessage(), e);
+                }
+            }
+        }
+    }
+
+    public Object valueFromBytes(byte[] bytes) {
+        if (bytes == null || bytes.length == 0) {
+            return null;
+        }
+
+        FSTObjectInput fstInput = null;
+        try {
+            fstInput = new FSTObjectInput(new ByteArrayInputStream(bytes));
+            return fstInput.readObject();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (fstInput != null) {
+                try {
+                    fstInput.close();
+                } catch (IOException e) {
+                    Log.get(getClass()).error(e.getMessage(), e);
+                }
+            }
+        }
+    }
 }
 
 
